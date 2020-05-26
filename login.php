11d74921962d84a38ad8dd5f0f74f575c0bad5a1
POPLBY <2359034214@qq.com>
2020-05-26 17:45:00 +0800

    poplby

---
+++ login.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>登录</title>
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
+    <div align="center">
+        <form action="#" method="post" >
+            <label for="fname">用户名：</label>
+            <input type="text" id="user" name="user" placeholder="用户名">
+            <br>
+            <label for="lname">密&nbsp&nbsp&nbsp&nbsp码：</label>
+            <input type="password" id="pwd" name="pwd" placeholder="密码">
+            <br>
+            <input type="submit" value="登录">
+            <br>
+            <a href="#">没有账号？点击注册！</a>
+        </form>
+    </div>
+</body>
+</html>
