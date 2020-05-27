8a9e66266b7d77988b65824fd01fac70fa18ac85
POPLBY <2359034214@qq.com>
2020-05-27 11:54:25 +0800

    poplby

--- conn/conn.php
+++ conn/conn.php
@@ -1,10 +1,29 @@
 <?php
 //根据自己的数据库账号密码等信息修改值
-$conn = mysqli_connect("localhost","root","123","db_ecoshop","3308") or die("数据库连接失败！".mysqli_error());
-mysqli_query($conn,"set names gb2312");
+class connDB {
+    private $conn;
+    private $host;      //数据库服务器主机名
+    private $user;      //数据库用户名
+    private $password;  //数据库密码
+    private $database;  //数据库名
+    private $port;      //数据库端口号
+    private $code;      //编码格式
+    function __construct($host,$user,$password,$database,$port,$code) {     //构造方法
+        $this->host = $host;
+        $this->user = $user;
+        $this->database = $database;
+        $this->password = $password;
+        $this->port = $port;
+        $this->code = $code;
+    }
+    function connect() {
+        $this->conn = mysqli_connect("$this->host","$this->user","$this->password","$this->database","$this->port") or die("连接数据失败！".mysqli_error());
+        mysqli_query($this->conn,"set names $this->code");
+        return $this->conn;
+    }
+}
 ?>
 
 //如果某页面需要连接数据库，请在使用前添加一下代码：
-<?php
 //include('conn/conn.php');
-?>
+
--- login.php
+++ login.php
@@ -1,26 +1,27 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>登录</title>
-    <link rel="stylesheet" type="text/css" href="css/login.css">
-</head>
-<body>
-    <div align="center">
-        <form action="#" method="post" >
-            <label for="fname">用户名：</label>
-            <input type="text" id="user" name="user" placeholder="用户名">
-            <br>
-            <label for="lname">密&nbsp&nbsp&nbsp&nbsp码：</label>
-            <input type="password" id="pwd" name="pwd" placeholder="密码">
-            <br>
-            <input type="submit" value="登录">
-            <br>
-            <a href="#">没有账号？点击注册！</a>
-        </form>
-    </div>
-</body>
-</html>
+<?php
+include("conn/conn.php");
+$type = $_POST['type'];
+$name = $_POST["user"];
+$pwd = $_POST["pwd"];
+$Db = new connDB('localhost','root','123','db_ecoshop','3308','utf-8');    //实例化connDb
+$conn = $Db->connect();                                                                                         //获取数据库连接对象
+//根据用户类型判断用户名和密码
+if($type == '0'){
+    $sql = mysqli_query($conn, "select * from tb_admin where name = '$name' and pwd = '$pwd'");     //管理员
+} else {
+    $sql = mysqli_query($conn, "select * from tb_user where name = '$name' and pwd = '$pwd'");      //用户
+}
+$finaly = mysqli_fetch_array($sql);     //将查询结果赋值
+if ($finaly > 0) {
+    session_start();
+    $_SESSION['user'] = $finaly;
+    //设置登录类型
+    if($type == '0') {
+        $_SESSION['type'] = 'admin';    //设置为管理员
+    }else {
+        $_SESSION['type'] = 'user';     //设置为普通用户
+    }
+    echo "<script>alert('欢迎登录！');location.href = 'index.php'</script>";
+} else {
+    echo "<script>alert('用户名或密码错误！');location.href='login.html'</script>";
+}
