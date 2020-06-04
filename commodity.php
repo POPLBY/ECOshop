74b014b212caeb501467385ac813b26c11a28fda
lzy <2276814769@qq.com>
2020-06-04 11:06:28 +0800

    lzy

--- commodity.php
+++ commodity.php
@@ -1,23 +1,15 @@
 <?php
-//include ("checkSession.php");
+header("Content-type: textml; charset=UTF-8");
 include ("libs/Smarty.class.php");
 $smarty = new Smarty();
 
 include("conn/conn.php");
-$Db = new connDB('localhost', 'root', '123', 'db_ecoshop', '3308', 'utf-8');    //实例化connDb
+$Db = new connDB('localhost', 'root', '', 'db_ecoshop', '3308', 'utf-8');    //实例化connDb
 $conn = $Db->getConnect();         //获取数据库连接对象
 
-$sql = "select * from `tb_shangpin` where id = 1";
+$sql = "select * from `tb_shangpin` where id = ".$_GET['id'];
 $result = mysqli_query($conn, $sql);
 $res = mysqli_fetch_row($result);
 $smarty->assign('row', $res);
-
-$spid = $_GET['id'];
-
-$sql = "select * from `tb_pingjia` where spid = $spid";
-$result = mysqli_query($conn, $sql);
-$res = mysqli_fetch_row($result);
-$smarty->assign('pj', $res);
-include ("header.php");
 $smarty->display('commodity.html');
 ?>
--- tuijian.php
+++ tuijian.php
@@ -9,6 +9,8 @@ $conn = $Db->getConnect();         //获取数据库连接对象
 
 $sql = "select * from `tb_shangpin` where `tuijian` = 1";
 $result = mysqli_query($conn, $sql);
+$ress=mysqli_num_rows($result);
+
 $arra= array();
 $n=0;
 while($res = mysqli_fetch_row($result))
@@ -16,11 +18,13 @@ while($res = mysqli_fetch_row($result))
 $arra[$n]=$res;
 $n++;
 }
+
 $sqls="select mingcheng from tb_shangpin where id=4";
 $re00=mysqli_query($conn,$sqls);
 $results=mysqli_fetch_row($re00);
 $smarty->assign('arra', json_encode($arra));
 $smarty->assign('k', $results[0]);
+$smarty->assign('len', $ress);
 $smarty->display('index.php');
 
 
